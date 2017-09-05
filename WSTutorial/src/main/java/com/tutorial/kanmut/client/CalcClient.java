e9d735693a3f2cb2b43c524b2496ac54e8eef0d7
InKaSa <InKaSa@AkInSaKa-PC>
2017-09-04 23:11:05 -0400

    Enhanced the client to pass muliply arguments via main method args

--- WSTutorial/src/main/java/com/tutorial/kanmut/client/CalcClient.java
+++ WSTutorial/src/main/java/com/tutorial/kanmut/client/CalcClient.java
@@ -1,28 +1,39 @@
 package com.tutorial.kanmut.client;
- 
+
 import java.net.URL;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
 import com.tutorial.kanmut.ws.Calc;
- 
-public class CalcClient{
- 
+
+public class CalcClient {
+
 	public static void main(String[] args) throws Exception {
- 
+
 		URL url = new URL("http://localhost:9999/ws/calc?wsdl");
- 
-        //1st argument service URI, refer to wsdl document above
-		//2nd argument is service name, refer to wsdl document above
-        QName qname = new QName("http://kanmut.tutorial.com/", "CalcImplService");
- 
-        Service service = Service.create(url, qname);
- 
-        Calc calc = service.getPort(Calc.class);
- 
-        System.out.println(calc.multiplyAsDouble(10d, 20d));
- 
-    }
- 
+
+		// 1st argument service URI, refer to wsdl document above
+		// 2nd argument is service name, refer to wsdl document above
+		QName qname = new QName("http://kanmut.tutorial.com/",
+				"CalcImplService");
+
+		Service service = Service.create(url, qname);
+
+		Calc calc = service.getPort(Calc.class);
+
+		if (args.length < 3) {
+			System.out.println("Usage:" + "\n"
+					+ "multiply multiplicand multiplier");
+		} else {
+			if ("multiply".equalsIgnoreCase(args[0])) {
+				try {
+					System.out.println(calc.multiplyAsDouble(
+							Double.valueOf(args[1]), Double.valueOf(args[2])));
+				} catch (NumberFormatException nfe) {
+					System.out.println("NumberFormatException");
+				}
+			}
+		}
+	}
 }
